c403991a2a147dec17b57e683815d5df54036725
andrewxzol <kubik13defence@gmail.com>
2026-03-08 23:11:41 +0200

    FIX: admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import GeminiImage
-print("ADMIN FILE LOADED")
-@admin.register(GeminiImage)
+
+# Видаляємо декоратор і реєструємо вручну
 class GeminiImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'image', 'created_at')
     readonly_fields = ('analysis_result', 'created_at')
+
+admin.site.register(GeminiImage, GeminiImageAdmin)
+print(f"!!! MODEL REGISTERED: {GeminiImage in admin.site._registry}")
--- core/settings.py
+++ core/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'api.apps.ApiConfig',
+    'rest_framework',
     'drf_spectacular',
     'django.contrib.admin',
     'django.contrib.auth',
