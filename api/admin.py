4282c2051ce476ccf92401bcc4110b39e7a7fd3e
andrewxzol <kubik13defence@gmail.com>
2026-03-08 22:58:39 +0200

    test

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import GeminiImage
-
+print("ADMIN FILE LOADED")
 @admin.register(GeminiImage)
 class GeminiImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'image', 'created_at')
