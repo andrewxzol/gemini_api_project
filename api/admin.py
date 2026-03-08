3ac52bb606a494d85ec02478262fd06d8887bff9
andrewxzol <kubik13defence@gmail.com>
2026-03-08 22:26:38 +0200

    UPD: add gemini image

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import GeminiImage
+
+@admin.register(GeminiImage)
+class GeminiImageAdmin(admin.ModelAdmin):
+    # Визначаємо, які колонки показувати в списку
+    list_display = ('id', 'image', 'created_at')
+    # Додаємо можливість перегляду результату аналізу
+    readonly_fields = ('analysis_result', 'created_at')
