6330611a6d3d34041fa901ca00625152f3f99aaa
andrewxzol <kubik13defence@gmail.com>
2026-03-10 15:25:52 +0200

    add static files

--- api/serializers.py
+++ api/serializers.py
@@ -21,7 +21,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         def create(self, validated_data):
             user = User.objects.create_user(
                 username=validated_data['username'],
-                email=validated_data('email', ''),
+                email=validated_data.get('email', ''),
                 password=validated_data['password']
             )
             return user
