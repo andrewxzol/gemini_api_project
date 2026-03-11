c14bf7ef075008a2736233136a0cf43ca2f540f1
andrewxzol <kubik13defence@gmail.com>
2026-03-11 11:17:30 +0200

    UPD: add new model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class GeminiImage(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='images',
+        null=True,  # Дозволяємо порожнє значення для існуючих картинок
+        blank=True
+    )
+
     image = models.ImageField(upload_to='uploaded_images/')
     analysis_result = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
